4f3ac084f57870e3d83b0efd7827871c744e9f64
pleskop <peter@evidy.cz>
2025-11-23 14:23:51 +0100

    refactor(glami): rename column `analytics_currency` to `system_currency`

--- models/sources/glami/source_glami__categories.sql
+++ models/sources/glami/source_glami__categories.sql
@@ -21,7 +21,7 @@ final as (
         _config_account_id as account_id,
         replace(replace(lower(_config_source_medium), ' ', ''), '/', '_')  as campaign_id,
 
-        currency_code as analytics_currency,
+        currency_code as system_currency,
 
         upper(_config_join_key) as key_name,
         upper(_config_name) as system_name,
