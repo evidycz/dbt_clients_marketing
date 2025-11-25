ebf8924e4c0a799041c96daee0d89ce0c3247a26
pleskop <peter.plesko@gmail.com>
2025-11-25 08:33:36 +0100

    Integrate purchase event metrics into campaign model (#9)
    * feat(marts): integrate purchase event metrics into campaign model
    
    Add a new `purchase_event` CTE to aggregate purchase-specific metrics, including `event_count` and `event_value`. Modify the join logic to use `purchase_event` instead of `events`, ensuring accurate representation of conversion data.
    
    * chore(project): bump version to 0.3.1
    
    ---------
    
    Co-authored-by: pleskop <peter@evidy.cz>

--- models/marts/intermediate/int_join_meta_campaigns_and_events.sql
+++ models/marts/intermediate/int_join_meta_campaigns_and_events.sql
@@ -16,6 +16,17 @@ events as (
     from {{ ref('source_meta_ads__events') }}
 ),
 
+purchase_event as (
+
+    select
+        parent_row_id,
+        sum(event_count) as event_count,
+        sum(event_value) as event_value
+    from events
+    where event_name = 'purchase'
+    group by 1
+)
+
 joined as (
 
     select
@@ -37,13 +48,13 @@ joined as (
 
         impressions,
         clicks,
-        event_count as conversions,
-        event_value as conversion_value,
+        coalesce(event_count, 0) as conversions,
+        coalesce(event_value, 0) as conversion_value,
         cost
 
     from campaigns
-    left join events
-        on campaigns.row_id = events.parent_row_id
+    left join purchase_event
+        on campaigns.row_id = purchase_event.parent_row_id
 )
 
 select * from joined
